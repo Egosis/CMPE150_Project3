10ac303d34aa5f390007ad75c0831c0c07af9b34
Egemen Isguder <45923068+Egosis@users.noreply.github.com>
2020-12-30 13:48:42 +0300

    Update solution_b.py

--- solution_b.py
+++ solution_b.py
@@ -1,7 +1,17 @@
+
 file=open("dataset.txt","r")
 document=file.read().split("\n")
+actors=[]        
 films=[]
+
 for i in range (0,len(document)):
-    films.append(document[i].split(",")[0])
-print(films)
+    splitted=document[i].split(",")
+    films.append(splitted[0])
+    for j in range(1,4):
+        if(splitted[j] not in actors):
+            actors.append(splitted[j])  
+            
+print(actors)
+
 file.close()
+
